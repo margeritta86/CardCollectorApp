bd6b5c738393b05db99ad058dee20aeb57e12a2a
margeritta86 <malgosia.glowiak@gmail.com>
2020-11-09 11:17:33 +0100

    first commit

---
+++ src/main/java/com/pokemon/app/CardCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardCollectorApplication.class, args);
+	}
+
+}
