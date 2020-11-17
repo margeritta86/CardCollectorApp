6e899dcd550cd9c7de680952d6dbb80882aaef70
margeritta86 <malgosia.glowiak@gmail.com>
2020-11-17 12:42:50 +0100

    commit simple register added

---
+++ src/main/java/com/pokemon/app/controller/RegisterController.java
@@ -0,0 +1,14 @@
+package com.pokemon.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping("/register")
+    public String getRegisterPage(){
+
+        return "register";
+    }
+}
